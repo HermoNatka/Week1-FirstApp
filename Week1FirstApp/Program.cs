e14720cf94b499b357e05cd96970052aee08a158
Hermo Natka <hermo.natka@tktk.ee>
2024-09-08 11:18:27 +0300

    add my first app

---
+++ Week1FirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string myName = "Hermo";
+string myLastName = "Natka";
+
+//Console.WriteLine("Hello" + " " + myName);
+
+Console.WriteLine($"Hello, {myName} {myLastName}!");
